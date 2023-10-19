a3096d611a37ff4131327723831cd7b9af8dc16f
ValenRCastro <valentina.rodriguez-01@outlook.com>
2023-10-18 20:56:02 -0500

    proyecto finalizado

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         btn_suma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txt_respuesta.setText(suma(Integer.parseInt(numero_1.getText().toString()), Integer.parseInt(numero_2.getText().toString()))+"");
+                txt_respuesta.setText(suma(Integer.parseInt(numero_1.getText().toString()), Integer.parseInt(numero_2.getText().toString())) + "");
             }
         });
 
@@ -39,15 +39,26 @@ public class MainActivity extends AppCompatActivity {
         btn_resta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txt_respuesta.setText(resta(Integer.parseInt(numero_1.getText().toString()), Integer.parseInt(numero_2.getText().toString()))+"");
+                txt_respuesta.setText(resta(Integer.parseInt(numero_1.getText().toString()), Integer.parseInt(numero_2.getText().toString())) + "");
             }
         });
 
         btn_multiplicacion = findViewById(R.id.btnMultiplicacion);
+        btn_multiplicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt_respuesta.setText(multiplicacion(Integer.parseInt(numero_1.getText().toString()), Integer.parseInt(numero_2.getText().toString())) + "");
+            }
+        });
 
 
         btn_division = findViewById(R.id.btnDivision);
-
+        btn_division.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt_respuesta.setText(division(Integer.parseInt(numero_1.getText().toString()), Integer.parseInt(numero_2.getText().toString())) + "");
+            }
+        });
 
         txt_respuesta = findViewById(R.id.respuesta);
 
@@ -55,19 +66,18 @@ public class MainActivity extends AppCompatActivity {
         numero_2 = findViewById(R.id.num2);
     }
 
-    public int suma (int a, int b) {
+    public int suma(int a, int b) {
         return a + b;
     }
 
-    public int resta (int a, int b) {
+    public int resta(int a, int b) {
         return a - b;
     }
 
-    public int multiplicacion (int a, int b) {
+    public int multiplicacion(int a, int b) {
         return a * b;
     }
 
-    public int division (int a, int b) {
-        return a / b;
-    }
+    public int division(int a, int b) {return a / b;}
+
 }
